643ae58546829ffcf64b238a6a2bc29283eec475
zambottinico <zambottinico@gmail.com>
2022-02-15 09:46:32 -0300

    se agrego una barra de carga y un icono a la pagina

--- codigos.js
+++ codigos.js
@@ -1,6 +1,10 @@
 const minar = document.getElementById("button");
+const carga = document.getElementById("carga")
 
 
+
+
+//mostrar xd
 mostrar = () => {
   document.getElementById("btn").innerHTML = btn;
   document.getElementById("info-minero").innerHTML =
@@ -8,6 +12,7 @@ mostrar = () => {
   document.getElementById("precio1").innerHTML = precio1;
   document.getElementById("precio2").innerHTML = precio2;
   document.getElementById("precio3").innerHTML = precio3;
+  
 };
 
 var precio1 = 50;
@@ -19,16 +24,42 @@ var minaAuto = 10;
 var intervalo = 10000;
 var intervaloMostrar = intervalo / 1000;
 
+mostrar();
+
 minar.addEventListener("click", (e) => {
   btn = btn + mina;
   mostrar();
 });
 
+//-----------BARRA DE CARGA----------------------
+
+const actualizarCarga =()=>{
+  carga.style.width = 100 + "%";
+  carga.style.transitionDuration = intervalo + "ms"
+  console.log("cargando")
+}
+
+const resetearBarra =()=>{
+  carga.style.transitionDuration = 0 + "ms"
+  carga.style.width = 0 + "%"
+  console.log("reseteando")
+}
+
+
+
+actualizarCarga()
 setInterval(() => {
-  {
+    
     btn = btn + minaAuto;
     mostrar();
-  }
+    resetearBarra();
+    setTimeout(() => {
+      actualizarCarga()
+      
+      console.log(intervalo)
+    }, 50);
+    
+  
 }, intervalo);
 
 //mejoras
